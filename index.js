0ac175f9e90281caf556ad40e9d231a9b4b90b43
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-04 18:51:20 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+window.customerName = 'bob';
+
+// Function to change customerName to uppercase
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBest) {
+  window.bestCustomer = newBest;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'some initial value';
+
+// Function to change leastFavoriteCustomer (should throw error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
